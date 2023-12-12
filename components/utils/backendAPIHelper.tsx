b7b6687032129eb0439a5f6dfb6c7b6249aae91e
azianmike <michaeluo@gmail.com>
2023-12-11 16:16:51 -0800

    add a new daniel radcliffe page

--- components/utils/backendAPIHelper.tsx
+++ components/utils/backendAPIHelper.tsx
@@ -16,3 +16,17 @@ export const saveNewuser = async () => {
     console.log("Got response")
     console.log(response)
 };
+
+export const generateImage = async (prompt : string) => {
+    const response = await fetch(`${URL_PREFIX}/generateImage`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ "inputs":prompt }),
+        credentials: 'include'
+    });
+    console.log("Got response")
+    console.log(response)
+    return response
+};
