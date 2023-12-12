32e07bb42ad492acda0e97d0362a3de3c872871a
azianmike <michaeluo@gmail.com>
2023-12-11 17:44:53 -0800

    Add in google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './css/style.css'
 
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 import Header from '@/components/ui/header'
 import Banner from '@/components/banner'
@@ -23,6 +24,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+    <Script src="https://www.googletagmanager.com/gtag/js?id=G-6WMRZ04BFL" />
+    <Script id="google-analytics">
+      {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-6WMRZ04BFL');
+        `}
+    </Script>
       <body className={`${inter.variable} font-inter antialiased bg-white text-gray-900 tracking-tight`}>
         <div className="flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip">
           <Header />
